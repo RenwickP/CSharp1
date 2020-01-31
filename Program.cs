cef123f84e1beb78d5fde0f819ca58e75f493443
renwickp <peter2renwick@gmail.com>
2020-01-30 18:35:27 -0700

    initial

--- Program.cs
+++ Program.cs
@@ -3,11 +3,15 @@ using ConsoleAdventure.Project;
 
 namespace ConsoleAdventure
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+  public class Program
+  {
+    public static void Main(string[] args)
 
-        }
+    {
+      Project.Controllers.GameController gc = new Project.Controllers.GameController();
+      gc.Run();
+
+
     }
+  }
 }
--- Project/Models/Game.cs
+++ Project/Models/Game.cs
@@ -2,15 +2,36 @@ using ConsoleAdventure.Project.Interfaces;
 
 namespace ConsoleAdventure.Project.Models
 {
-    public class Game : IGame
-    {
-        public IRoom CurrentRoom { get; set; }
-        public IPlayer CurrentPlayer { get; set; }
+  public class Game : IGame
+  {
+    public IRoom CurrentRoom { get; set; }
+    public IPlayer CurrentPlayer { get; set; }
 
-        //NOTE Make yo rooms here...
-        public void Setup()
-        {
-            throw new System.NotImplementedException();
-        }
+    //NOTE Make yo rooms here...
+    public void Setup()
+    {
+      Room One = new Room("One", "This is the first room you enter. Its pretty cool but what is the point of anything really? Welcome to your own existential morass. Perhaps exploration will give you meaning...");
+
+      Room Two = new Room("Two", "Oh cool another room...");
+
+      Room Three = new Room("Three", "This is a square room just like all the rest. Perhaps you can find something worth doing in this one.");
+
+      Room Four = new Room("Four", "Its your choice win or die in this room");
+
+      One.InnerRoom = Two;
+      Two.OuterRoom = One;
+      Two.InnerRoom = Three;
+      Three.InnerRoom = Four;
+      Three.OuterRoom = Two;
+      Four.OuterRoom = Three;
+
+
+      CurrentRoom = One;
+
     }
+    public Game()
+    {
+      Setup();
+    }
+  }
 }
